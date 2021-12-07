f6767bc79855651582bf101636892b993110d20f
siecie <sige1751@colorado.edu>
2021-12-07 13:53:48 -0700

    push

--- Final/index.js
+++ Final/index.js
@@ -4,13 +4,13 @@ let btn2 = document.querySelector('#btn2');
 let btn3 = document.querySelector('#btn3');
 
 btn1.addEventListener('click', () => {
-  img.src= 'Media Gallery/Blossum.jpg';
+  <img.src= 'Media Gallery/Blossum.jpg';
 })
 
 btn2.addEventListener('click', () => {
-  img.src= 'Media Gallery/Crested Butte.jpg';
+  <img.src= 'Media Gallery/Crested Butte.jpg';
 })
 
 btn3.addEventListener('click', () => {
-  img.src= 'Media Gallery/Flatirons.jpg';
+  <img.src= 'Media Gallery/Flatirons.jpg';
 })
