777182ce0f2ed96a94e93b64216d9cdb6f27c3af
siecie <sige1751@colorado.edu>
2021-12-09 15:38:44 -0700

    btns

--- Final/index.js
+++ Final/index.js
@@ -2,21 +2,39 @@ let img = document.querySelector('#blossum');
 let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
 let btn3 = document.querySelector('#btn3');
-
+var prev = document.getElementById('btn1');
 btn1.addEventListener('click', () => {
   img.src= 'Media Gallery/Blossum.jpg';
+  var selected = document.getElementById('btn1');
+  btn1.style.backgroundColor = "white";
+  btn2.style.backgroundColor = "black";
+  btn3.style.backgroundColor = "black";
+  selected.backgroundColor = "white";
+  prev = selected;
 })
 
 btn2.addEventListener('click', () => {
   img.src= 'Media Gallery/Crested Butte.jpg';
+  var selected = document.getElementById('btn1');
+  btn2.style.backgroundColor = "white";
+  btn1.style.backgroundColor = "black";
+  btn3.style.backgroundColor = "black";
+  selected.backgroundColor = "white";
+  prev = selected;
 })
 
 btn3.addEventListener('click', () => {
   img.src= 'Media Gallery/Flatirons.jpg';
+  var selected = document.getElementById('btn1');
+  btn3.style.backgroundColor = "white";
+  btn2.style.backgroundColor = "black";
+  btn1.style.backgroundColor = "black";
+  selected.backgroundColor = "white";
+  prev = selected;
 })
 
-var buttons = $('button');
-buttons.click(function() {
-  buttons.css('background-color', 'black');
-  $(this).css('background-color', 'white');
-});
+// var buttons = $('button');
+// buttons.click(function() {
+//   buttons.css('background-color', 'black');
+//   $(this).css('background-color', 'white');
+// });
